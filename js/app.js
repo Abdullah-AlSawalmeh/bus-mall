57f30f41ead540643bf8e55ebf6853726c051d40
Abdullah-AlSawalmeh <abdullahalsawalmeh@gmail.com>
2021-03-15 06:54:03 +0200

    start again

---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+/////////////////////////// Globals ///////////////////
+let imgs_array = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg']
+
+
+
+function Product (product_name) {
+
+
+}
