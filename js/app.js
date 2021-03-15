2a0a392508f1f664edbcc269c15622a82dbed254
Abdullah-AlSawalmeh <abdullahalsawalmeh@gmail.com>
2021-03-15 08:24:28 +0200

    Still in progress

--- js/app.js
+++ js/app.js
@@ -23,36 +23,34 @@ let imgs_array = [
   ['water-can', 'jpg'],
   ['wine-glass', 'jpg']
 ];
-
-
-
-
+//////////////////// Constructer /////////////////////////////
 function Product (product_name) {
   this.product_name = product_name[0];
   this.file_path = './img/' + product_name[0] +'.'+ product_name[1];
   this.image_shown_counter = 0; 
 }
 
-Product.prototype.random = function () {
-  let min = 0;
-  let max = imgs_array.length;
-  let random_image = Math.floor(Math.random() * (max - min) + min);
-  return random_image;
-};
-
 Product.prototype.render = function() {
   // console.log(this.product_name);
   // console.log(this.file_path);
   let img = document.createElement('img');
-  
-
-
+  img.src = this.file_path;
+  section.appendChild(img);
 };
+//////////////////// End Of Constructer /////////////////////////////
 
-for (let i = 0; i < imgs_array.length; i++) {
-  let imgobj = new Product(imgs_array[i]);
+
+//////////////Funtion to generate 3 random images on the page////////
+function random_number () {
+  let min = 0;
+  let max = imgs_array.length;
+  let random_image = Math.floor(Math.random() * (max - min) + min);
+  return random_image;
+}
+
+for (let i = 0; i < 3; i++) {
+  let imgobj = new Product(imgs_array[random_number()]);
   imgobj.render();
-
 }
 
 
